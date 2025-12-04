f1182caea37e94b82426f057f23b635f7e026685
Lucas <lucas.silveira630521@gmail.com>
2025-12-04 17:16:31 -0300

    Primeiro commit

---
+++ jornada-milhas2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { BannerComponent } from "./banner/banner.component";
+import { FormBuscaPassagensComponent } from "./form-busca-passagens/form-busca-passagens.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, BannerComponent, FormBuscaPassagensComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'jornada-milhas';
+}
