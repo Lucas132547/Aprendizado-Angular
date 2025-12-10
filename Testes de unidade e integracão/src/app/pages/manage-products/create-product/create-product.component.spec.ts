a0bb1120523019c6e04c570fa87df7be2a5b7b2a
Lucas <lucas.silveira630521@gmail.com>
2025-12-10 17:25:01 -0300

    Mudancas e Inicio de Smart Component

--- Testes de unidade e integracão/src/app/pages/manage-products/create-product/create-product.component.spec.ts
+++ Testes de unidade e integracão/src/app/pages/manage-products/create-product/create-product.component.spec.ts
@@ -1,14 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateProductComponent } from './create-product.component';
+import { CommonModule } from '@angular/common';
 
-describe('CreateProductComponent', () => {
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+fdescribe('CreateProductComponent', () => {
   let component: CreateProductComponent;
   let fixture: ComponentFixture<CreateProductComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateProductComponent]
+      imports: [
+        CreateProductComponent,
+        CommonModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatIconModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef, useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA, useValue: {}
+        }
+      ]
     })
     .compileComponents();
     
