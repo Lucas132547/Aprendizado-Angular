5b2ddc43400b559892537a56b14dca7ed874b2a2
Lucas <lucas.silveira630521@gmail.com>
2025-12-10 14:40:26 -0300

    Mudancas

--- Testes de unidade e integracão/src/app/shared/components/card/card.component.spec.ts
+++ Testes de unidade e integracão/src/app/shared/components/card/card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { Product } from '../../../types/product.inteface';
+import { By } from '@angular/platform-browser';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -20,4 +22,32 @@ describe('CardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the product in the template',() => {
+    const product: Product = {
+      id: 1,
+      title: 'iPhone 15',
+      price: 1000,
+      category: 'eletronico',
+      description: 'Smartphone',
+      image: 'src/assets/image/png'
+    }
+
+    component.product = product;
+    fixture.detectChanges();
+    
+
+    const productImg = fixture.debugElement.query(By.css('img')).nativeElement;
+    const productTitle = fixture.debugElement.query(By.css('h2')).nativeElement;
+    const productDescription = fixture.debugElement.query(By.css('p')).nativeElement;
+    const productPrice = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+    expect(productImg.src).toContain(product.image);
+    expect(productTitle.textContent).toContain(product.title);
+    expect(productDescription.textContent).toContain(product.description);
+    expect(productPrice.textContent).toContain(product.price);
+
+
+
+  })
 });
