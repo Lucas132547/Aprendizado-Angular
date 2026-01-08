3419c1249bb4fe626d8c60a49973653534f564ce
Lucas Silveira <65314805+Lucas132547@users.noreply.github.com>
2026-01-08 10:55:16 -0300

    Finalizando autenticacao protecao e maniulacao de rotas

---
+++ Autenticaçao proteçao e manipulaçao de rotas/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private isAuthenticated = signal <boolean>(false);
+
+  login() {
+    this.isAuthenticated.set(true); 
+  }
+
+  logout() {
+    this.isAuthenticated.set(false);
+  }
+
+  isloggedIn() {
+    return this.isAuthenticated();
+  }
+
+  constructor() { }
+}
